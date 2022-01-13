f40cabf9f2a6422fcd74a23a4299ee7f40e59d02
eligarlo <eligarlo.developer@gmail.com>
2022-01-13 06:50:34 +0200

    Added Fade Animation on FeedbackItem

--- src/components/FeedbackList.tsx
+++ src/components/FeedbackList.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import FeedbackItem from './FeedbackItem'
 import { IFeedbackData } from '../utils/SharedUtils'
+import { motion, AnimatePresence } from 'framer-motion'
 
 interface IFeedbackListProps {
   feedbacks: IFeedbackData[]
@@ -11,13 +12,31 @@ const FeedbackList: React.FC<IFeedbackListProps> = ({ feedbacks, handleDelete })
   if (!feedbacks || feedbacks.length === 0) {
     return <p>No Feedback Yet.</p>
   }
+
   return (
     <div className='feedback-list'>
-      {feedbacks.map(feedback => (
-        <FeedbackItem key={feedback.id} feedback={feedback} handleDelete={handleDelete} />
-      ))}
+      <AnimatePresence>
+        {feedbacks.map(feedback => (
+          <motion.div
+            key={feedback.id}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            <FeedbackItem key={feedback.id} feedback={feedback} handleDelete={handleDelete} />
+          </motion.div>
+        ))}
+      </AnimatePresence>
     </div>
   )
+
+  // return (
+  //   <div className='feedback-list'>
+  //     {feedbacks.map(feedback => (
+  //       <FeedbackItem key={feedback.id} feedback={feedback} handleDelete={handleDelete} />
+  //     ))}
+  //   </div>
+  // )
 }
 
 export default React.memo(FeedbackList)
