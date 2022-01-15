a17d71a237626f0701a2f0cc89f66a2430aafb48
eligarlo <eligarlo.developer@gmail.com>
2022-01-15 22:45:25 +0200

    Added Spinner component

--- src/components/FeedbackList.tsx
+++ src/components/FeedbackList.tsx
@@ -3,27 +3,31 @@ import { useContext } from 'react'
 import FeedbackContext from 'context/feedback/FeedbackContext'
 import { motion, AnimatePresence } from 'framer-motion'
 import FeedbackItem from 'components/FeedbackItem'
+import Spinner from 'components/shared/Spinner'
 
 const FeedbackList: React.FC = () => {
-  const { feedbacks } = useContext(FeedbackContext)
+  const { feedbacks, isLoading } = useContext(FeedbackContext)
 
-  if (!feedbacks || feedbacks.length === 0) {
+  if (!isLoading && (!feedbacks || feedbacks.length === 0)) {
     return <p>No Feedback Yet.</p>
   }
 
-  return (
+  return isLoading ? (
+    <Spinner />
+  ) : (
     <div className='feedback-list'>
       <AnimatePresence>
-        {feedbacks.map(feedback => (
-          <motion.div
-            key={feedback.id}
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-          >
-            <FeedbackItem key={feedback.id} feedback={feedback} />
-          </motion.div>
-        ))}
+        {feedbacks &&
+          feedbacks.map(feedback => (
+            <motion.div
+              key={feedback.id}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+            >
+              <FeedbackItem key={feedback.id} feedback={feedback} />
+            </motion.div>
+          ))}
       </AnimatePresence>
     </div>
   )
---
+++ src/components/shared/Spinner.tsx
@@ -0,0 +1,12 @@
+import spinner from 'components/assets/spinner.gif'
+const Spinner: React.FC = () => {
+  return (
+    <img
+      src={spinner}
+      alt='Loading...'
+      style={{ width: '100px', margin: 'auto', display: 'block' }}
+    />
+  )
+}
+
+export default Spinner
